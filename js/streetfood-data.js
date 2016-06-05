ae7d9fe6550f5057def426efd4860758c8fb85b1
Celine Solenthaler <c.solenthaler@gmail.com>
2016-06-05 19:59:06 +0200

    Imported latest json

--- js/streetfood-data.js
+++ js/streetfood-data.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-    $.getJSON("result_streetfood.json", function(foods) {
-        $.each(foods, function(index, data) {
-            var food = data[0];
+    $.getJSON("result_streetfood.json", function(data) {
+        $.each(data.foods, function(index, food) {
             var element = $("" +
                 "<div class='streetfood'>" +
                 "   <a href='streetfood.html'>" +
-                "       <img src='" + food['image']['source'] + "'>" +
+                "       <img src='" + food['image']['source'] + "' class='img'>" +
                 "       <div class='text1'>" + food['name'] + "</div>" +
                 "   </a>" +
                 "</div>");
